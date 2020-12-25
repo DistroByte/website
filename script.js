0ff1dbe266d8c283be5022bc376d548af417de92
James Hackett <jamesthackett1@gmail.com>
2020-12-25 05:14:02 +0000

    :sparkles: Feat: clears text boxes on reload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // window.onbeforeunload = function () {
 //   return "Two buttons will be below this message asking if the user wants to stay on this page or leave.";
 // }
+
+window.onload = function () {
+  document.getElementById('contact-message').value = '';
+}
